2efe5f81c7fd88804942aa1945b9006528b063eb
danielkuiper1 <9015587@student.tcrmbo.nl>
2023-06-15 11:31:27 +0200

    commit

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>login and registration</h1>
+    <h2>login here</h2>
+    <form>
+        Username:<input type="name"></br>
+        Password:<input type="password"></br>
+        <input type="button" value="Login">
+    </form>
+    <a href="registration.php">registration</a>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>registration</h1>
+    <h2>Make an account here</h2>
+    <form>
+        Username:<input type="name"></br>
+        Password:<input type="password"></br>
+        <input type="button" value="make account">
+    </form>
+</body>
+</html>
